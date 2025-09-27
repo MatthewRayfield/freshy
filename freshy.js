a160f2d39b5bbdac16de4a668202c18bffedef21
Matthew Rayfield <MatthewRayfield@MatthewRayfield.com>
2025-09-27 10:38:34 -0400

    bug fixes + clean. someimtes i'm bad at code

--- freshy.js
+++ freshy.js
@@ -1,45 +1,43 @@
-(async () => {
-    let last = {};
-    let checks = [location.href];
-    let delay = 520;
+let last = {};
+let checks = [location.href];
+let delay = 520;
 
-    function wait(t) {
-        return new Promise(resolve => {
-            setTimeout(resolve, t);
-        });
+function wait(t) {
+    return new Promise(resolve => {
+        setTimeout(resolve, t);
+    });
+}
+
+async function init() {
+    for (const ele of document.querySelectorAll('[freshy]')) {
+        checks.push(ele.src || ele.href);
     }
 
-    async function init() {
-        for (const ele of document.querySelectorAll('[freshy]')) {
-            checks.push(ele.src || ele.href);
-        }
+    console.info(`freshy v1.0.2 \u{1F624} ${checks.length} \u{1F440}`)
+    //console.debug(JSON.stringify(checks, 0, 4));
 
-        console.info(`freshy v0.1.1 \u{1F624} ${checks.length} \u{1F440}`)
-        //console.debug(JSON.stringify(checks, 0, 4));
+    while (true) {
+        for (let url of checks) {
+            const resp = await fetch(url, {method: 'HEAD'});
+            const lmh = resp.headers.get('Last-Modified');
 
-        while (true) {
-            for (let url of checks) {
-                const resp = await fetch(url, {method: 'HEAD'});
-                const lmh = resp.headers.get('Last-Modified');
-
-                if (last[url] != undefined && lmh != last[url]) {
-                    location.reload();
-                }
-
-                last[url] = lmh;
+            if (last[url] != undefined && lmh != last[url]) {
+                location.reload();
             }
 
-            await wait(delay);
+            last[url] = lmh;
         }
+
+        await wait(delay);
     }
+}
 
-    globalThis.freshy = {
-        wait,
-        init,
-        last,
-        checks,
-        delay
-    };
+globalThis.freshy = {
+    wait,
+    init,
+    last,
+    checks,
+    delay
+};
 
-    freshy.init();
-})();
+onload = freshy.init;
